08b28b92835ae94101be8e4eb0d46c13527739df
Wei-liang Huang <William@Wei-liangs-Air.hsd1.wa.comcast.net>
2014-11-16 18:59:45 -0800

    4

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 /*jshint node: true*/
 'use strict';
 
-$.getJSON("//ip-api.com/json/?callback=?", function(data) {
+$.getJSON("https//ip-api.com/json/?callback=?", function(data) {
   var ip = data.query;
   $.post( "/api/"+ip, function( data ) {
     $( ".message" ).html( '<h3>' + data + '</h3>');
