56dd883739c64feb5418869002071c91f230eb74
Wei-liang Huang <William@Wei-liangs-Air.hsd1.wa.comcast.net>
2014-11-16 19:16:26 -0800

    9

--- public/main.js
+++ public/main.js
@@ -1,9 +1,8 @@
 /*jshint node: true*/
 'use strict';
 
-$.getJSON("https://freegeoip.net/json", function(data) {
-  var ip = data.ip;
-  console.log(ip);
+$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+  var ip = data.query;
   $.post( "/api/"+ip, function( data ) {
     $( ".message" ).html( '<h3>' + data + '</h3>');
   });
