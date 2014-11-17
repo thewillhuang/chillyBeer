ee09a567d3dca51db29dd4f77c268e4116e54a9f
Wei-liang Huang <William@Wei-liangs-Air.hsd1.wa.comcast.net>
2014-11-16 18:35:32 -0800

    init commit

---
+++ public/main.js
@@ -0,0 +1,9 @@
+/*jshint node: true*/
+'use strict';
+
+$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+  var ip = data.query;
+  $.post( "/api/"+ip, function( data ) {
+    $( ".message" ).html( '<h3>' + data + '</h3>');
+  });
+});
